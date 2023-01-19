b8ed9b5420ffbdb8723ad4c95bee7c4fbf628d93
wwng2333 <wwng@2333.me>
2023-01-19 15:25:24 +0800

    Update DoH.php

--- DoH.php
+++ DoH.php
@@ -1,40 +1,40 @@
 <?php
-//composer install workerman
-//bump to 4.x
 use Workerman\Worker;
 use Workerman\Connection\TcpConnection;
 use Workerman\Protocols\Http\Request;
 use Workerman\Protocols\Http\Response;
+
 require_once __DIR__ . '/vendor/autoload.php';
 define('DOH_UPSTREAM', 'https://1.1.1.1/dns-query');
+define('ENDPOINT_PATH', '/dns-query');
+define('START_MODE', 'HTTPS'); //HTTP or HTTPS
+define('DEBUG', 1);
 
-// HTTPS 模式
-$context = array(
-    'ssl' => array(
-        'local_cert'        => '/root/1.cer', // 也可以是crt文件
-        'local_pk'          => '/root/1.key',
-        'verify_peer'       => false,
-        'allow_self_signed' => false, //如果是自签名证书需要开启此选项
-    )
-);
-$http_worker = new Worker('http://0.0.0.0:2345', $context);
-$http_worker->transport = 'ssl';
-
-// HTTP 模式
-//$worker->onMessage = function(TcpConnection $con, $msg) {
-//    $con->send('ok');
-//};
-//$http_worker = new Worker("http://0.0.0.0:2345");
+if(START_MODE == 'HTTPS')
+{
+	$context = array(
+		'ssl' => array(
+			'local_cert'        => '/root/1.cer', // 也可以是crt文件
+			'local_pk'          => '/root/1.key',
+			'verify_peer'       => false,
+			'allow_self_signed' => false, //如果是自签名证书需要开启此选项
+		)
+	);
+	$http_worker = new Worker('http://0.0.0.0:2345', $context);
+	$http_worker->transport = 'ssl';
+} else {
+	$http_worker = new Worker("http://0.0.0.0:2345");
+}
 
 $http_worker->count = 4;
 $http_worker->name = 'DoH Proxy';
 $http_worker->onMessage = function(TcpConnection $connection, Request $request)
 {	
+	if(DEBUG) printf("recv:%s, %s at %s\n", $request->method(), $request->header('accept'), $request->uri());
 	$response_400 = new Response(400, [
 			'Content-Type' => 'text/plain; charset=utf-8'
 		], 'Bad Request');
-	//var_dump($data);
-	if($request->uri()=='/dns-query' and $request->rawBody()!='')
+	if($request->uri()==ENDPOINT_PATH and !empty($request->rawBody()))
 	{
 		$curl = curl_init();
 		curl_setopt($curl, CURLOPT_URL, DOH_UPSTREAM);
@@ -43,8 +43,6 @@ $http_worker->onMessage = function(TcpConnection $connection, Request $request)
 		curl_setopt($curl, CURLOPT_HTTPHEADER, array("Content-Type: application/dns-message"));
 		$res = curl_exec($curl);
 		curl_close($curl);
-		//var_dump('post', base64_encode($request->rawBody()));
-		
 		$response = new Response(200, [
 			'Content-Type' => 'application/dns-message'
 		], $res);
@@ -55,25 +53,21 @@ $http_worker->onMessage = function(TcpConnection $connection, Request $request)
 		$t = explode('dns-query', $request->uri());
 		$query_url = DOH_UPSTREAM.$t[1];
 		
-		//var_dump($request->header('accept'));
 		switch(strtolower($request->header('accept')))
 		{
 			case "application/dns-json":
 				$is_json = 1;
 				$out = dns_query($query_url, 'json');
-				//var_dump('dns-json: '.$query_url);
 			break;
 			
 			case "application/dns-message":
-				$is_json = 0;
 				$out = dns_query($query_url, 'dns');
-				//var_dump('dns-msg: '.$query_url);
 			break;
 			
 			default: return $connection->close($response_400);
 		}
 		
-		if($is_json) $header = array(["Content-Type: application/json; charset=UTF-8"]);
+		if(!empty($is_json)) $header = array(["Content-Type: application/json; charset=UTF-8"]);
 		else $header = array(["Content-Type: application/dns-message"]);
 
 		$response = new Response(200, $header, $out);
@@ -82,12 +76,9 @@ $http_worker->onMessage = function(TcpConnection $connection, Request $request)
 	else return $connection->close($response_400);
 };
 
-Worker::runAll();
-
 function dns_query($url, $method)
 {
-	if($method == 'dns') $headerArray = array("Accept: application/dns-message");
-	else $headerArray = array("Accept: application/dns-json");
+	$headerArray = ($method == 'dns') ? array("Accept: application/dns-message") : array("Accept: application/dns-json");
 	$ch = curl_init();
 	curl_setopt($ch, CURLOPT_URL, $url);
 	curl_setopt($ch, CURLOPT_TIMEOUT_MS, 1000);
@@ -97,3 +88,5 @@ function dns_query($url, $method)
 	curl_close($ch);
 	return $output;
 }
+
+Worker::runAll();
