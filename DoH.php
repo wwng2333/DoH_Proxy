93330957b49b6a907d44b5ec9e918591b2641482
wwng2333 <wwng@2333.me>
2023-01-21 12:28:57 +0800

    Update DoH.php

--- DoH.php
+++ DoH.php
@@ -6,7 +6,7 @@ use Workerman\Protocols\Http\Response;
 use WpOrg\Requests\Requests;
 
 require_once __DIR__ . '/vendor/autoload.php';
-define('DOH_UPSTREAM', 'https://223.6.6.6/dns-query');
+define('DOH_UPSTREAM', 'https://1.1.1.1/dns-query');
 define('ENDPOINT_PATH', '/dns-query');
 define('START_MODE', 'HTTPS'); //HTTP or HTTPS
 define('DEBUG', 1);
@@ -27,27 +27,22 @@ if(START_MODE == 'HTTPS')
 	$http_worker = new Worker("http://0.0.0.0:2345");
 }
 
-$http_worker->count = 10;
+$http_worker->count = 6;
 $http_worker->name = 'DoH Proxy';
 $http_worker->onMessage = function(TcpConnection $connection, Request $request)
 {
 	if($request->method()== 'POST' and $request->uri()==ENDPOINT_PATH and !empty($request->rawBody()))
 	{
-		if(DEBUG) printf("POST %s %s\n", $request->header('accept'), base64_encode($request->rawBody()));
-		$post = Requests::post(DOH_UPSTREAM, ['accept' => $request->header('accept'), 'content-type' => $request->header('content-type')], $request->rawBody());
-		if(DEBUG) printf("recv %s\n", base64_encode($post->body));
-		return $connection->close(new Response(200, ['Content-Type' => $request->header('accept')], $post->body));
+		//if(DEBUG) printf("POST %s %s\n", $request->header('accept'), base64_encode($request->rawBody()));
+		$res = Requests::post(DOH_UPSTREAM, ['accept' => $request->header('accept'), 'content-type' => $request->header('content-type')], $request->rawBody());
 	} 
 	else if($request->method() == 'GET' and stristr($request->uri(), ENDPOINT_PATH.'?') and !empty($request->header('accept')))
 	{
-		if(DEBUG) printf("GET %s %s\n", $request->header('accept'), $request->uri());
-		$Return_header = ['Content-Type' => $request->header('accept')];
-		if(stristr($request->header('accept'), 'json')) $Return_header['Content-Type'] .= '; charset=utf-8';
+		//if(DEBUG) printf("GET %s %s\n", $request->header('accept'), $request->uri());
 		$t = explode('dns-query', $request->uri());
-		$get = Requests::get(DOH_UPSTREAM.$t[1], ['Accept' => $request->header('accept')]);
-		if(DEBUG) printf("recv %s\n", base64_encode($get->body));
-		return $connection->send(new Response(200, ['Content-Type' => $get->headers['content-type']], $get->body));
+		$res = Requests::get(DOH_UPSTREAM.$t[1], ['accept' => $request->header('accept')]);
 	}
+	if($res->success) return $connection->send(new Response(200, ['Content-Type' => $res->headers['content-type']], $res->body));
 	return $connection->close(new Response(400, ['Content-Type' => 'text/plain; charset=utf-8'], 'Bad Request'));
 };
 
