085ac017a78542f68d70d8bff17122edef4fe52d
wwng2333 <wwng@2333.me>
2023-01-21 13:51:03 +0800

    Update DoH.php
    The respon will have currect Content-Length now

--- DoH.php
+++ DoH.php
@@ -8,7 +8,7 @@ use WpOrg\Requests\Requests;
 require_once __DIR__ . '/vendor/autoload.php';
 define('DOH_UPSTREAM', 'https://1.1.1.1/dns-query');
 define('ENDPOINT_PATH', '/dns-query');
-define('START_MODE', 'HTTPS'); //HTTP or HTTPS
+define('START_MODE', 'HTTP'); //HTTP or HTTPS
 define('DEBUG', 1);
 
 if(START_MODE == 'HTTPS')
@@ -27,14 +27,17 @@ if(START_MODE == 'HTTPS')
 	$http_worker = new Worker("http://0.0.0.0:2345");
 }
 
-$http_worker->count = 6;
+$http_worker->count = 4	;
 $http_worker->name = 'DoH Proxy';
 $http_worker->onMessage = function(TcpConnection $connection, Request $request)
 {
 	if($request->method()== 'POST' and $request->uri()==ENDPOINT_PATH and !empty($request->rawBody()))
 	{
-		//if(DEBUG) printf("POST %s %s\n", $request->header('accept'), base64_encode($request->rawBody()));
 		$res = Requests::post(DOH_UPSTREAM, ['accept' => $request->header('accept'), 'content-type' => $request->header('content-type')], $request->rawBody());
+		if(DEBUG) {
+			printf("POST %s %s\n", $request->header('accept'), base64_encode($request->rawBody()));
+			var_dump($res->headers);
+		}
 	} 
 	else if($request->method() == 'GET' and stristr($request->uri(), ENDPOINT_PATH.'?') and !empty($request->header('accept')))
 	{
@@ -42,7 +45,10 @@ $http_worker->onMessage = function(TcpConnection $connection, Request $request)
 		$t = explode('dns-query', $request->uri());
 		$res = Requests::get(DOH_UPSTREAM.$t[1], ['accept' => $request->header('accept')]);
 	}
-	if($res->success) return $connection->send(new Response(200, ['Content-Type' => $res->headers['content-type']], $res->body));
+	if($res->success) return $connection->send(new Response(200, [
+		'Content-Type' => $res->headers['Content-Type'],
+		'Content-Length' => $res->headers['Content-Length']
+	], $res->body));
 	return $connection->close(new Response(400, ['Content-Type' => 'text/plain; charset=utf-8'], 'Bad Request'));
 };
 
