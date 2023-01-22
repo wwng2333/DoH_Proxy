a8d9fcb85789230a872a34199f4dc2ccf8716825
wwng2333 <wwng@2333.me>
2023-01-22 17:00:12 +0800

    udp: init
    support udp upstream

--- DoH.php
+++ DoH.php
@@ -6,19 +6,20 @@ use Workerman\Protocols\Http\Response;
 use Workerman\Worker;
 use WpOrg\Requests\Requests;
 
-require_once __DIR__.'/vendor/autoload.php';
+require_once __DIR__ . '/vendor/autoload.php';
 define('DOH_UPSTREAM', 'https://1.1.1.1/dns-query');
+#define('DOH_UPSTREAM', 'udp://223.6.6.6:53');
 define('ENDPOINT_PATH', '/dns-query');
-define('START_MODE', 'HTTP'); //HTTP or HTTPS
+define('START_MODE', 'HTTPS'); //HTTP or HTTPS
 define('DEBUG', 1);
 
 if (START_MODE == 'HTTPS') {
     $http_worker = new Worker('http://0.0.0.0:2345', [
         'ssl' => [
-            'local_cert' => '/root/1.cer', // 也可以是crt文件
+            'local_cert' => '/root/1.cer',
             'local_pk' => '/root/1.key',
             'verify_peer' => false,
-            'allow_self_signed' => false, //如果是自签名证书需要开启此选项
+            'allow_self_signed' => false,
         ],
     ]);
     $http_worker->transport = 'ssl';
@@ -29,23 +30,37 @@ if (START_MODE == 'HTTPS') {
 $http_worker->count = 4;
 $http_worker->name = 'DoH Proxy';
 $http_worker->onMessage = function (TcpConnection $connection, Request $request) {
-    if ($request->method() == 'POST' and $request->uri() == ENDPOINT_PATH and ! empty($request->rawBody())) {
+    $url = parse_url(DOH_UPSTREAM);
+
+    if ($request->method() == 'POST' and $request->uri() == ENDPOINT_PATH and !empty($request->rawBody())) {
+        $t_fin = $request->rawBody();
+    } elseif ($request->method() == 'GET' and stristr($request->uri(), ENDPOINT_PATH . '?') and !empty($request->header('accept'))) {
+        $t = explode('?dns=', $request->uri());
+        $t_fin = base64_decode($t[1]);
+    }
+    var_dump(base64_encode($t_fin));
+    if ($url['scheme'] == 'udp') {
+        if (!isset($url['port']))
+            $url['port'] = 53;
+        $fp = fsockopen('udp://' . $url['host'], $url['port'], $errno, $errstr, 1);
+        fwrite($fp, $t_fin);
+        $r_body = fread($fp, 8192);
+        $r_flag = ($errno == 0) ? 1 : 0;
+    } elseif (stristr($url['scheme'], 'http')) {
         $res = Requests::post(DOH_UPSTREAM, [
             'accept' => $request->header('accept'),
-            'content-type' => $request->header('content-type'),
-        ], $request->rawBody());
-    } elseif ($request->method() == 'GET' and stristr($request->uri(), ENDPOINT_PATH.'?') and ! empty($request->header('accept'))) {
-        $t = explode('dns-query', $request->uri());
-        $res = Requests::get(DOH_UPSTREAM.$t[1], ['accept' => $request->header('accept')]);
+            'content-type' => $request->header('accept'),
+        ], $t_fin);
+        $r_flag = $res->success;
+        $r_body = $res->body;
     }
-    if ($res->success) {
+
+    if ($r_flag) {
         return $connection->send(new Response(200, [
-            'Content-Type' => $res->headers['Content-Type'],
-            'Content-Length' => $res->headers['Content-Length'],
-        ], $res->body));
+            'Content-Type' => 'application/dns-message',
+        ], $r_body));
     }
 
     return $connection->close(new Response(400, ['Content-Type' => 'text/plain; charset=utf-8'], 'Bad Request'));
 };
-
 Worker::runAll();
